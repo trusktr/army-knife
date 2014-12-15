084404024bc0e83996724c7d5f6e0aa6a521556b
Joseph Orbegoso Pea <joseph.pea@climate.com>
2014-12-15 00:09:23 -0800

    Initial commit.

---
+++ Object.className.js
@@ -0,0 +1,8 @@
+import "javascripts/utils/polyfill.Function.name";
+
+Object.defineProperty(Object.prototype, 'className', {
+  enumerable: false,
+  get: function() {
+      return this.constructor.name || "Object";
+  }
+});
