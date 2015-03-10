06c5c0e39d75683160d7641da4b8efef54527f18
Joseph Orbegoso Pea <joe@trusktr.io>
2015-03-10 16:48:33 -0700

    Not using underscore.

--- src/bindWhenFunction.js
+++ src/bindWhenFunction.js
@@ -2,7 +2,7 @@
 // If the first arg to bindWhenFunction is a function, bind it to context.
 export function bindWhenFunction(possibleFunction, context) {
     if (typeof possibleFunction === 'function') {
-        possibleFunction = _(possibleFunction).bind(context);
+        possibleFunction = possibleFunction.bind(context);
     }
 
     return possibleFunction;
